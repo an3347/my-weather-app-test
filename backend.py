1f37be6e8b541bb85968c7927232e59e2d1405b7
an3347 <160431021+an3347@users.noreply.github.com>
2024-06-10 16:44:27 -0400

    added os

---
+++ backend.py
@@ -0,0 +1,22 @@
+import requests
+import os
+
+API_KEY = os.getenv("weather_api_key")
+
+
+def get_data(place, forcast_days):
+    try:
+        url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+        response = requests.get(url=url)
+        data = response.json()
+        filtered_data = data["list"]
+        nr_values = 8 * forcast_days
+        filtered_data = filtered_data[:nr_values]
+        return filtered_data
+    except KeyError:
+        print(f"City {place} not found")
+
+
+
+if __name__ == "__main__":
+    print(get_data(place="TorontoA", forcast_days=3))
