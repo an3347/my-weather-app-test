47b00c8e04fbb9cd0a502762f09887feb9389a31
an3347 <akshat.nagpal6@gmail.com>
2024-06-10 14:34:27 -0400

    completed

--- backend.py
+++ backend.py
@@ -3,19 +3,19 @@ import requests
 API_KEY = "7147511e0613505f05495615ba5fe57c"
 
 
-def get_data(place, forcast_days, kind):
-    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
-    response = requests.get(url=url)
-    data = response.json()
-    filtered_data = data["list"]
-    nr_values = 8 * forcast_days
-    filtered_data = filtered_data[:nr_values]
-    if kind == "Temperature":
-        filtered_data = [d["main"]["temp"] for d in filtered_data]
-    elif kind == "Sky":
-        filtered_data = [d["weather"][0]["main"] for d in filtered_data]
-    return data
+def get_data(place, forcast_days):
+    try:
+        url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+        response = requests.get(url=url)
+        data = response.json()
+        filtered_data = data["list"]
+        nr_values = 8 * forcast_days
+        filtered_data = filtered_data[:nr_values]
+        return filtered_data
+    except KeyError:
+        print(f"City {place} not found")
+
 
 
 if __name__ == "__main__":
-    print(get_data(place="Toronto", forcast_days=3, kind="Temperature"))
+    print(get_data(place="TorontoA", forcast_days=3))
--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
+# Add title, text input, slider, selection-box and sub-header
 st.title("Weather Forecast for the Next Days")
 place = st.text_input("Place: ")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select number of forecast days")
 option = st.selectbox("Select data to view",
-                      ("Temperature", "sky"))
+                      ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
+if place:
+    try:
+        # Get temperature/sky data
+        filtered_data = get_data(place=place, forcast_days=days)
 
-def get_data(days):
-    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
-    temperatures = [10, 17, 20]
-    temperatures = [i * days for i in temperatures]
-    return dates, temperatures
+        if option == "Temperature":
+            temperature = [dic["main"]["temp"] / 10 for dic in filtered_data]
+            dates = [dic["dt_txt"] for dic in filtered_data]
+            # create a temperature plot
+            figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
 
-
-d, t = get_data(days)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear": "./images/clear.png", "Clouds": "./images/cloud.png", "Rain": "./images/rain.png",
+                      "Snow": "./images/snow.png"}
+            sky_conditions = [d["weather"][0]["main"] for d in filtered_data]
+            image_paths = [images[condition] for condition in sky_conditions]
+            print(sky_conditions)
+            st.image(image_paths, width=115)
+    except TypeError:
+        st.info(f"City name \"{place}\" is not valid")
